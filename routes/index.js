5bc5c17bdbb870a01dbc755104faa1d4a188b37f
momogc <172766829@qq.com>
2018-02-07 14:08:41 +0800

    add api REST

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
   app.use('/signout', require('./signout'))
   app.use('/posts', require('./posts'))
   app.use('/comments', require('./comments'))
+  app.use('/api', require('./api'))
   // 404 page
   app.use(function (req, res) {
     if (!res.headersSent) {
