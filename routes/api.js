f5b225bca3b8c555ae15512a420934f790c88a2c
momogc <172766829@qq.com>
2018-02-09 16:42:52 +0800

    test ajax post formdata (blob, string)

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const BASE_DIR = __dirname
 router.post('/', jsonParser, function (req, res) {
   console.log(req.body)
@@ -10,6 +12,13 @@ router.post('/', jsonParser, function (req, res) {
     'b': 2
   })
 })
+router.post('/form', upload.single('file'), function (req, res) {
+  console.log(req.body)
+  res.send({
+    'a': 1,
+    'b': 2
+  })
+}) // ajax send formdata (blob, string)
 router.get('/test', function (req, res) {
   res.sendFile(BASE_DIR + '/todie.html')
 })
